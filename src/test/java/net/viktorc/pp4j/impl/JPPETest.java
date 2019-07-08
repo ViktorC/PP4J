e099bbac090b41f36007bc2f36626082d86b7874
Viktor Csomor <viktor.csomor@gmail.com>
2019-07-08 15:29:39 +0100

    Relax performance requirements

--- src/test/java/net/viktorc/pp4j/impl/JPPETest.java
+++ src/test/java/net/viktorc/pp4j/impl/JPPETest.java
@@ -625,7 +625,7 @@ public class JPPETest {
         res.get();
       }
       long time = System.currentTimeMillis() - start;
-      boolean success = time < 15000 && time > 7500;
+      boolean success = time < 18000 && time > 7500;
       System.out.printf("Time: %.3f %s%n", ((double) time) / 1000, success ? "" : "FAIL");
       Assert.assertTrue(success);
     } finally {
