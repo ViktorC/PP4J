29a67cf862ccf11debc26cca21af6143c827673b
Viktor Csomor <viktor.csomor@gmail.com>
2019-08-25 01:50:45 +0100

    Add unit tests for JavaSubmission

--- src/test/java/net/viktorc/pp4j/impl/JavaSubmissionTest.java
+++ src/test/java/net/viktorc/pp4j/impl/JavaSubmissionTest.java
@@ -15,7 +15,19 @@
  */
 package net.viktorc.pp4j.impl;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.io.UncheckedIOException;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
+import net.viktorc.pp4j.api.Command;
+import net.viktorc.pp4j.api.FailedCommandException;
+import net.viktorc.pp4j.impl.JavaProcess.Response;
+import net.viktorc.pp4j.impl.JavaProcess.ResponseType;
+import net.viktorc.pp4j.impl.JavaSubmission.SerializableTask;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -25,6 +37,119 @@ import org.junit.Test;
  */
 public class JavaSubmissionTest extends TestCase {
 
+  @Test
+  public void testThrowsUncheckedIOExceptionIfNotSerializable() {
+    Object nonSerializableObject = new Object();
+    exceptionRule.expect(UncheckedIOException.class);
+    new JavaSubmission<>((Runnable & Serializable) () -> System.out.println(nonSerializableObject));
+  }
 
+  @Test
+  public void testTaskMakesRunnableCallable() throws Exception {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) () -> System.out.println("hi"));
+    Object result = javaSubmission.getTask().call();
+    Assert.assertSame(null, result);
+  }
+
+  @Test
+  public void testTaskThrowsRuntimeExceptionAsRunnable() {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Callable<Integer> & Serializable) () -> {
+      throw new Exception();
+    });
+    exceptionRule.expect(RuntimeException.class);
+    javaSubmission.getTask().run();
+  }
+
+  @Test
+  public void testTaskReturnsCorrectValue() throws Exception {
+    JavaSubmission<Integer> javaSubmission = new JavaSubmission<>((Callable<Integer> & Serializable) () -> 1);
+    SerializableTask<Integer> task = javaSubmission.getTask();
+    Assert.assertEquals(new Integer(1), task.call());
+  }
+
+  @Test
+  public void testTaskIncrementsAtomicInteger() {
+    AtomicInteger atomicInteger = new AtomicInteger(0);
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) atomicInteger::incrementAndGet);
+    javaSubmission.getTask().run();
+    Assert.assertEquals(1, atomicInteger.get());
+  }
+
+  @Test
+  public void testGetCommandsReturnsSingleCommand() {
+    JavaSubmission<?> submission = new JavaSubmission<>((Runnable & Serializable) () -> {});
+    Assert.assertEquals(1, submission.getCommands().size());
+  }
+
+  @Test
+  public void testDecodedCommandInstruction() throws Exception {
+    AtomicInteger atomicInteger = new AtomicInteger(5);
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Callable<Integer> & Serializable) atomicInteger::incrementAndGet);
+    Command command = javaSubmission.getCommands().get(0);
+    String instruction = command.getInstruction();
+    Object decodedCommandInstruction = JavaObjectCodec.getInstance().decode(instruction);
+    Assert.assertTrue(decodedCommandInstruction instanceof Callable);
+    Callable<?> task = (Callable<?>) decodedCommandInstruction;
+    Assert.assertEquals(6, task.call());
+    Assert.assertEquals(5, atomicInteger.get());
+  }
+
+  @Test
+  public void testCommandIsCompletedReturnsFalseIfStringNotDecodable() throws FailedCommandException {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) () -> {});
+    Command command = javaSubmission.getCommands().get(0);
+    Assert.assertFalse(command.isCompleted("", false));
+  }
+
+  @Test
+  public void testCommandIsCompletedReturnsFalseIfLineFromStdErr() throws IOException, FailedCommandException {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) () -> {});
+    Command command = javaSubmission.getCommands().get(0);
+    Response response = new Response(ResponseType.TASK_SUCCESS);
+    String outputLine = JavaObjectCodec.getInstance().encode(response);
+    Assert.assertFalse(command.isCompleted(outputLine, true));
+  }
+
+  @Test
+  public void testCommandIsCompletedReturnsFalseIfResponseProcessFailure() throws IOException, FailedCommandException {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) () -> {});
+    Command command = javaSubmission.getCommands().get(0);
+    Response response = new Response(ResponseType.PROCESS_FAILURE);
+    String outputLine = JavaObjectCodec.getInstance().encode(response);
+    Assert.assertFalse(command.isCompleted(outputLine, true));
+  }
+
+  @Test
+  public void testCommandIsCompletedThrowsFailedCommandExceptionIfResponseTaskFailure() throws IOException, FailedCommandException {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) () -> {});
+    Command command = javaSubmission.getCommands().get(0);
+    Response response = new Response(ResponseType.TASK_FAILURE);
+    String outputLine = JavaObjectCodec.getInstance().encode(response);
+    exceptionRule.expect(FailedCommandException.class);
+    command.isCompleted(outputLine, false);
+  }
+
+  @Test
+  public void testCommandIsCompletedThrowsFailedCommandExceptionWithCorrectCauseIfResponseTaskFailure() throws IOException,
+      FailedCommandException {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) () -> {});
+    Command command = javaSubmission.getCommands().get(0);
+    Response response = new Response(ResponseType.TASK_FAILURE, new IllegalArgumentException());
+    String outputLine = JavaObjectCodec.getInstance().encode(response);
+    exceptionRule.expect(FailedCommandException.class);
+    exceptionRule.expectCause(CoreMatchers.isA(IllegalArgumentException.class));
+    command.isCompleted(outputLine, false);
+  }
+
+  @Test
+  public void testCommandIsCompletedReturnsTrueAndSetsResultIfTaskSuccess() throws IOException, FailedCommandException {
+    JavaSubmission<?> javaSubmission = new JavaSubmission<>((Runnable & Serializable) () -> {});
+    Command command = javaSubmission.getCommands().get(0);
+    Response response = new Response(ResponseType.TASK_SUCCESS, 5);
+    String outputLine = JavaObjectCodec.getInstance().encode(response);
+    Assert.assertEquals(Optional.empty(), javaSubmission.getResult());
+    Assert.assertTrue(command.isCompleted(outputLine, false));
+    Assert.assertEquals(5, javaSubmission.getResult().orElse(null));
+  }
 
 }
